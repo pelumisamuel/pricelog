6762446b54de13fac8ebe27ab98a9b8b762f018e
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-07-21 09:23:01 +0100

    restructure send email function and add Email pass credential to .env

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -1,18 +1,19 @@
 import nodemailer from 'nodemailer'
 import { emailToken } from './generateToken.js'
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+})
+
 const sendVerificationLink = async (email, id) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      port: 465,
-
-      auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: 'vuzujstbquzgvyak',
-      },
-    })
     const url = `http://localhost:3000/api/users/confirmation/${emailToken(id)}`
     await transporter.sendMail({
       from: 'price log <priceloggger@gmail.com>',
